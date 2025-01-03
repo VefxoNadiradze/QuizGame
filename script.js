77f9786b5752717a4deed1306840ab5f340876a3
VefxoNadiradze <olld00@bk.ru>
2025-01-03 21:11:07 +0400

    add click event on the next btn, increasing the currentIndex and change the html element innertexts

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const currentIndexSpan = document.querySelector(".currentIndexSpan");
 const QuizLengthSpan = document.querySelector(".QuizLengthSpan");
 const question = document.querySelector(".question");
 const answersParent = document.querySelector(".answers");
+const nextBtn = document.querySelector(".nextBtn");
 let currentIndex = 0;
 const answers = ["A", "B", "C", "D"];
 
@@ -12,11 +13,23 @@ const getData = async () => {
     currentIndexSpan.innerText = currentIndex + 1;
     QuizLengthSpan.innerText = data.length;
 
-    answers.map((item, index) => {
+    answers.map((item) => {
       const answer = document.createElement("button");
       answer.classList.add("answer");
       answersParent.appendChild(answer);
       answer.innerText = data[currentIndex][item];
+    });
+
+    nextBtn.addEventListener("click", () => {
+      if (currentIndex + 1 < data.length) {
+        currentIndexSpan.innerText = ++currentIndex + 1;
+
+        let answerBtns = Array.from(document.querySelectorAll(".answer"));
+
+        answers.map((item, index) => {
+          answerBtns[index].innerText = data[currentIndex][item];
+        });
+      }
     });
   } catch (error) {
     console.log(error);
