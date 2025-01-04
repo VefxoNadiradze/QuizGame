e018065bc5141e909d634255abb4e4bb918eb7f8
VefxoNadiradze <olld00@bk.ru>
2025-01-04 19:55:34 +0400

    add countdown function if count is zero updating data

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const QuizLengthSpan = document.querySelector(".QuizLengthSpan");
 const question = document.querySelector(".question");
 const answersParent = document.querySelector(".answers");
 const nextBtn = document.querySelector(".nextBtn");
+const countDownSpan = document.querySelector(".countDownSpan");
 let currentIndex = 0;
 const answers = ["A", "B", "C", "D"];
-
+let count = 10;
+let interval;
 const disableAnswerButtons = () => {
   let answerBtns = Array.from(document.querySelectorAll(".answer"));
   answerBtns.map((item) => {
@@ -20,6 +22,39 @@ const getData = async () => {
     currentIndexSpan.innerText = currentIndex + 1;
     QuizLengthSpan.innerText = data.length;
     question.innerText = data[currentIndex].question;
+
+    const CountDownFoo = () => {
+      clearInterval(interval);
+      count = 10;
+      countDownSpan.innerText = count;
+
+      interval = setInterval(() => {
+        console.log(count);
+
+        if (count > 0) {
+          countDownSpan.innerText = count;
+          count--;
+        } else {
+          countDownSpan.innerText = count;
+          clearInterval(interval);
+          currentIndexSpan.innerText = ++currentIndex + 1;
+          question.innerText = data[currentIndex].question;
+          let answerBtns = Array.from(document.querySelectorAll(".answer"));
+          answers.map((item, index) => {
+            answerBtns[index].innerText = data[currentIndex][item];
+            answerBtns[index].classList.remove("correctAnswer");
+            answerBtns[index].classList.remove("IncorrectAnswer");
+            answerBtns[index].disabled = false;
+            if (currentIndex + 1 < data.length) {
+              CountDownFoo();
+            }
+          });
+        }
+      }, 1000);
+    };
+
+    CountDownFoo();
+
     answers.map((item) => {
       const answer = document.createElement("button");
       answer.classList.add("answer");
@@ -40,7 +75,7 @@ const getData = async () => {
       if (currentIndex + 1 < data.length) {
         currentIndexSpan.innerText = ++currentIndex + 1;
         question.innerText = data[currentIndex].question;
-
+        CountDownFoo();
         let answerBtns = Array.from(document.querySelectorAll(".answer"));
 
         answers.map((item, index) => {
